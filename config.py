db5448ab29d9a47a6377c86d2f2ba0def6666521
Doug Mahugh <dmahugh@google.com>
2019-12-16 07:57:13 -0800

    add comments to config file

--- config.py
+++ config.py
@@ -1,6 +1,12 @@
-# Configuration settings for the pyodbc connection string.
+"""Configuration settings for the pyodbc connection string.
 
-# Set this to the name of an ODBC driver installed on your local machine.
+This file should be in the same folder as the pyodbc_snippets.ipynb file.
+
+If you're only planning to browse the notebook in read-only mode, you
+don't need to modify any of the settings in this file.
+"""
+
+# The name of an ODBC driver installed on your local machine.
 driver = "ODBC Driver 17 for SQL Server"
 
 # The sample notebook uses the Cloud SQL Proxy running locally.
@@ -9,6 +15,5 @@ server = "127.0.0.1"
 # The default admin user for a SQL Server instance is named sqlserver.
 sql_user = "sqlserver"
 
-# This is the password specified during creation of the
-# Cloud SQL for SQL Server instance.
+# The password specified during creation of the Cloud SQL for SQL Server instance.
 sql_password = "ENTER-YOUR-PASSWORD-HERE"
