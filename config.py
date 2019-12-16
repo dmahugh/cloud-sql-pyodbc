8659b557de8099336ebb97c2aecc9c068ad7f820
Doug Mahugh <dmahugh@google.com>
2019-12-16 07:50:39 -0800

    add config file

---
+++ config.py
@@ -0,0 +1,16 @@
+# PRIVATE configuration settings. DON'T PUSH TO GITHUB.
+
+# The driver setting contains the name of an ODBC driver installed on your
+# local machine.
+driver = "ODBC Driver 17 for SQL Server"
+
+# The sample notebook uses the Cloud SQL Proxy running locally, so the
+# server is 127.0.0.1.
+server = "127.0.0.1"
+
+# The default admin user for a SQL Server instance is named sqlserver.
+sql_user = "sqlserver"
+
+# Change this setting to the password you specified during creation of the
+# Cloud SQL for SQL Server instance.
+sql_password = "ENTER-YOUR-PASSWORD-HERE"
